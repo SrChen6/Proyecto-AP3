3091c503a9a24c64d05742891f67d00d5700d4bc
SrChen6 <haokangchen6@gmail.com>
2024-11-29 12:00:48 +0100

    exh coords to be ckeched

--- exh.cc
+++ exh.cc
@@ -4,6 +4,7 @@
 #include <fstream> //Lectura de archivos
 #include <map> //Equivalente a diciconarios de python
 #include <chrono> //Cronometra el tiempo de ejecución
+#include <algorithm>
 using namespace std;
 
 typedef pair<int, int> Pair; //Tuplas
@@ -11,30 +12,31 @@ typedef map<Pair, int>    Map; //Diccionarios
 typedef pair<Pair, Pair>     Coords; // Posición de una pieza en la solución
 typedef vector<Coords>     VectCoords; //Conjunto de piezas posicionadas
 
-
+// GLOBALS 
 int W, N; //Anchura del telar y numero de comandas
 Map n; //Dimensiones + numero de piezas
-int L=100; //Longitud ans parcial
-int best_L;
+int L=0; //Longitud ans parcial
+int best_L; //Inicialitzat a main()
 VectCoords disp = {}; //disposicion de ans parcial/total
 VectCoords best_disp = {};
-int k; // Numero de piezas que falta añadir
+
+// Declaración de funciones
+void exh_search(char** argv, vector<int>& front, int k);
 
 // Inicio de cronómetro
 auto start = chrono::steady_clock::now();
 
-void read_instance(char** file) {
-  ifstream in(file[1]);
-  in >> W >> N;
+void read_instance(char** file, int& k) {
+  ifstream inp(file[1]);
+  inp >> W >> N;
   k = N;
   int ni, pi, qi;
   while (N != 0) {
-    in >> ni >> pi >> qi;
+    inp >> ni >> pi >> qi;
     N -= ni;
     n[{pi, qi}] = ni;
   }
-  cout << "W: "<<W<<" N: "<<k<<endl;
-
+  std::cout << "W: "<<W<<" N: "<<k<<endl; //TEMPORAL: PARA FACILITAR DEBUGGING
 }
 
 void write_ans(char** argv, double elapsed_seconds){
@@ -46,22 +48,79 @@ void write_ans(char** argv, double elapsed_seconds){
   }
 }
 
-void exh_search(char** argv, vector<int> front){
-  if(k=0){ //Si ha añadido todas las piezas 
-    if (L < best_L){
-      best_L = L;
-      // best_disp = disp;
+// No borrado por si metemos más podas 
+bool valid(){
+  return L < best_L;
+}
 
-      auto end = chrono::steady_clock::now();
-      auto elapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
-      double elapsed_seconds = elapsed.count() / 1000.0;
+void undo_change(vector<int>& front, VectCoords& disp){
+  Coords pos_piece = disp.back();
+  cout << "pieza quitada " << pos_piece.first.first << pos_piece.first.second<<endl;
+  disp.pop_back();
 
-      //Escribir la solución
-      write_ans(argv, elapsed_seconds);
-      }
+  Pair ul = pos_piece.first;
+  Pair dr = pos_piece.second;
+  Pair piece = {dr.first-ul.first, dr.second-ul.second};
+  n[piece] += 1;
+}
+
+void add_piece(Pair p, vector<int>& front, char** argv, int k){
+  n[p] -=1; // Quitar la pieza de que sea libre
+  int a = p.first;
+  bool may_add_here;
+  
+  for (int i=0; i<=W-a; ++i){ //Mirar cada casilla posible
+    may_add_here = true;
+    int j = 0;
+    while (j <a && may_add_here){
+      may_add_here = may_add_here && (front[i] <= front[i+j]);
+      ++j;
+    }
+
+    if (may_add_here){ //Añadir la pieza
+      for (int j=0; j<a; ++j) front[i+j]+=p.second ;
+      L = *max_element(front.cbegin(), front.cend());
+      disp.push_back({{i, front[i]},{i+p.first, front[i]+p.second}});
+      cout << "added piece " << p.first << p.second << endl;
+      exh_search(argv, front, k-1);
+      undo_change(front, disp);
+    }
+  }
+}
+
+void exh_search(char** argv, vector<int>& front, int k){
+  if(k==0){ //Si ha añadido todas las piezas 
+    best_L = L;
+    best_disp = disp;
+    cout << "best_L" << L <<endl;
+    cout << "L " << L << endl;
+
+    // Mira el tiempo
+    auto end = chrono::steady_clock::now();
+    auto elapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
+    double elapsed_seconds = elapsed.count() / 1000.0;
+
+    //Escribir la solución
+    write_ans(argv, elapsed_seconds);
   }
   else{
-    //Caso recursivo
+    if(valid()){ //poda
+      cout << "k index" << k<< endl;
+      for(pair<Pair, int> bloc : n){
+        if (bloc.second > 0) //Si quedan piezas de la dimension
+        {
+          cout << "entra a caso rec" << endl;
+          if(bloc.first.first == bloc.first.second){ // Si la pieza es cuadrada
+            add_piece(bloc.first, front, argv, k); // update front, disp
+          }
+          else{
+            add_piece(bloc.first, front, argv, k);
+            //Caso rotado
+            add_piece({bloc.first.second, bloc.first.first},front, argv, k);
+          }
+        }
+      }
+    }
   }
 }
 
@@ -74,14 +133,20 @@ int main(int argc, char** argv) {
     exit(0);
   }
 
+  int k; // Contador recursiu
   assert(argc == 3);
-
-  read_instance(argv);
+  read_instance(argv, k);
+  
+  // Calcular una cota superior para realizar podas
+  for(pair<Pair, int> bloc : n){
+    best_L += bloc.second * min(bloc.first.first, bloc.first.second);
+  }
+  cout << "best L " <<best_L <<endl;
 
   // Longitud a la que se ha llegado en cada columna
   vector<int> front(W, 0); 
 
-  exh_search(argv, front);
+  exh_search(argv, front, k);
 
 
 }
--- greedy.cc
+++ greedy.cc
@@ -4,6 +4,7 @@
 #include <fstream> //Lectura de archivos
 #include <map> //Equivalente a diciconarios de python
 #include <chrono> //Cronometra el tiempo de ejecución
+#include <algorithm>
 using namespace std;
 
 typedef pair<int, int> Pair; //Tuplas
@@ -11,68 +12,116 @@ typedef map<Pair, int>    Map; //Diccionarios
 typedef pair<Pair, Pair>     Coords; // Posición de una pieza en la solución
 typedef vector<Coords>     VectCoords; //Conjunto de piezas posicionadas
 
-void read_instance(const char* file) {
-  ifstream in(file);
-  Pair Telar;
-  in >> Telar.first >> Telar.second;
-  Map n;
+// GLOBALS 
+int W, N; //Anchura del telar y numero de comandas
+Map n; //Dimensiones + numero de piezas
+int L=999999; //Longitud ans parcial
+int best_L = 0;
+VectCoords disp = {}; //disposicion de ans parcial/total
+VectCoords best_disp = {};
+
+// Inicio de cronómetro
+auto start = chrono::steady_clock::now();
+
+void read_instance(char** file, int& k) {
+  ifstream inp(file[1]);
+  inp >> W >> N;
+  k = N;
   int ni, pi, qi;
-  while (Telar.second != 0) {
-    in >> ni >> pi >> qi;
-    Telar.second -= ni;
+  while (N != 0) {
+    inp >> ni >> pi >> qi;
+    N -= ni;
     n[{pi, qi}] = ni;
   }
+  std::cout << "W: "<<W<<" N: "<<k<<endl; //TEMPORAL: PARA FACILITAR DEBUGGING
+
 }
 
-void write_ans(string argv, double elapsed_seconds, int L, VectCoords disposicion){
-  //TODO: reset output file
-  ofstream outp(argv);
-  outp << elapsed_seconds << endl << L << endl;
-  for (pair bloc : disposicion){
+void write_ans(char** argv, double elapsed_seconds){
+  ofstream outp(argv[2]);
+  outp << elapsed_seconds << endl << best_L << endl;
+  for (Coords bloc : best_disp){
     outp << bloc.first.first << " " << bloc.first.second << " ";
     outp << bloc.second.first << " " << bloc.second.second << endl;
   }
 }
 
+// No borrado por si metemos más podas 
+bool valid(){
+  return L < best_L;
+}
+
+// void add_piece(Pair p, vector<int> front){
+//   n[p] -=1; // Quitar la pieza de que sea libre
+//   int a = p.first
+  
+//   for (int i=0; i<=W-a, ++i){
+//     if (std::all_of(front.cbegin(), front.cend(), [](int j) { return j <=i; }))
+//   }
+// }
+
+void undo_changes(vector<int> front, VectCoords disp){
+  Coords pos_piece = disp.pop_back();
+  Pair ul = pos_piece.first;
+  Pair 
+}
+
+void exh_search(char** argv, vector<int> front, int k){
+  if(k==0){ //Si ha añadido todas las piezas 
+    best_L = L;
+    best_disp = disp;
+
+    auto end = chrono::steady_clock::now();
+    auto elapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
+    double elapsed_seconds = elapsed.count() / 1000.0;
+
+    //Escribir la solución
+    write_ans(argv, elapsed_seconds);
+  }
+  else{
+    if(valid()){ //poda
+      for(pair<Pair, int> bloc : n){
+        if (bloc.second > 0) //Si quedan piezas de la dimension
+        {
+          if(bloc.first.first == bloc.first.second){ // Si la pieza es cuadrada
+            add_piece(bloc.first, front); // update front, disp
+          }
+          else{
+            add_piece(bloc.first, front);
+            undo_changes(front, disp);
+            
+            add_piece({bloc.first.second,bloc.first.first},front);
+          }
+          exh_search(argv, front, k-1);
+          undo_changes(front, disp);
+        }
+      }
+    }
+  }
+}
+
 int main(int argc, char** argv) {
 
-  // Escritura de formato de ejecución
+  // Formato de ejecución
   if (argc == 1) {
     cout << "Makes a sanity check of a solution" << endl;
     cout << "Usage: " << argv[0] << " INPUT_FILE OUTPUT_FILE" << endl;
     exit(0);
   }
+
+  int k; // Contador recursiu
   assert(argc == 3);
-
-  // Inicio de cronómetro
-  auto start = chrono::steady_clock::now();
-
-  // Apertura de archivos
-  ofstream outp(argv[2]);
-  ifstream inp(argv[1]);
-
-  // Variables de entrada y salida
-  Pair Telar;
-  Map n;
-
-  int L;
-  Coords pieza = {{0,0}, {1,1}};
-  VectCoords disposicion = {pieza};
+  read_instance(argv, k);
   
-  // Lectura de archivo de entrada
-  inp >> Telar.first >> Telar.second;
-  int ni, pi, qi;
-  while (Telar.second != 0) {
-    inp >> ni >> pi >> qi;
-    Telar.second -= ni;
-    n[{pi, qi}] = ni;
+  // Calcular una cota superior para realizar podas
+  for(pair<Pair, int> bloc : n){
+    best_L += bloc.second * min(bloc.first.first, bloc.first.second);
   }
 
-  // Finalización del cronómetro
-  auto end = chrono::steady_clock::now();
-  auto elapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
-  double elapsed_seconds = elapsed.count() / 1000.0;
-  write_ans(argv[2], elapsed_seconds, L, disposicion);
-  outp.close();
-  cout << "Executat correctament";
+  // Longitud a la que se ha llegado en cada columna
+  vector<int> front(W, 0); 
+
+  exh_search(argv, front, k);
+
+
 }
