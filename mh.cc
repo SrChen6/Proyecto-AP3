df6e7790ec81cc25af92ef4439110b13c0c62654
EKI <ekiciclofractal636@gmail.com>
2024-12-24 17:53:23 +0100

    inicio meth

--- mh.cc
+++ mh.cc
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 #include <assert.h> 
@@ -7,8 +8,19 @@
 #include <algorithm>
 using namespace std;
 
+// Idea: Reordenar el vector de piezas según grande a pequeño y generar dos vectores : una con la mitad grande 
+// y otra con la mitad pequeña. 
+
 typedef pair<int, int> Pair; //Tuplas
-typedef map<Pair, int>    Map; //Diccionarios
+struct CompareByFirst {
+    bool operator()(const Pair& a, const Pair& b) const {
+        if (a.first != b.first) {
+            return a.first > b.first; // Compare first elements
+        }
+        return a.second < b.second; // If first elements are equal, compare second elements
+    }
+};
+typedef map<Pair, int, CompareByFirst>    Map; //Diccionarios
 typedef pair<Pair, Pair>     Coords; // Posición de una pieza en la solución
 typedef vector<Coords>     VectCoords; //Conjunto de piezas posicionadas
 
@@ -16,39 +28,41 @@ typedef vector<Coords>     VectCoords; //Conjunto de piezas posicionadas
 int W, N; //Anchura del telar y numero de comandas
 Map n; //Dimensiones + numero de piezas
 int L=999999; //Longitud ans parcial
-int best_L = 0;
 VectCoords disp = {}; //disposicion de ans parcial/total
-VectCoords best_disp = {};
+
+// Optimization globals
+vector< Pair > n_vect = {} ; // Lista con piezas (incluyendo repetidas) ordenada
+vector< Pair > n_res; // Piezas pequeñas no colocadas en la primera pasada
+vector< Pair > blocs_to_put = {} ; 
+
 
 // Inicio de cronómetro
 auto start = chrono::steady_clock::now();
 
-void read_instance(char** file, int& k) {
+void read_instance(char** file) {
   ifstream inp(file[1]);
   inp >> W >> N;
-  k = N;
   int ni, pi, qi;
   while (N != 0) {
     inp >> ni >> pi >> qi;
     N -= ni;
     n[{pi, qi}] = ni;
   }
-  std::cout << "W: "<<W<<" N: "<<k<<endl; //TEMPORAL: PARA FACILITAR DEBUGGING
-
+  std::cout << "W: "<<W<<" N: "<<endl; //TEMPORAL: PARA FACILITAR DEBUGGING
+  for (const auto& p : n){ cout <<n[{p.first.first, p.first.second}]<<" "<< p.first.first << " " <<p.first.second<<endl;}
 }
 
 void write_ans(char** argv, double elapsed_seconds){
   ofstream outp(argv[2]);
-  outp << elapsed_seconds << endl << best_L << endl;
-  for (Coords bloc : best_disp){
+  outp << elapsed_seconds << endl << L << endl;
+  cout << elapsed_seconds << endl << "L :" << L << endl;
+  for (Coords bloc : disp){
     outp << bloc.first.first << " " << bloc.first.second << " ";
     outp << bloc.second.first << " " << bloc.second.second << endl;
+    cout << bloc.first.first << " " << bloc.first.second << " ";
+    cout << bloc.second.first << " " << bloc.second.second << endl;
   }
-}
-
-// No borrado por si metemos más podas 
-bool valid(){
-  return L < best_L;
+  cout <<endl;
 }
 
 // void add_piece(Pair p, vector<int> front){
@@ -60,44 +74,60 @@ bool valid(){
 //   }
 // }
 
-void undo_changes(vector<int> front, VectCoords disp){
-  Coords pos_piece = disp.pop_back();
-  Pair ul = pos_piece.first;
-  Pair 
+bool compareBySecond(const pair<int, int>& a, const pair<int, int>& b) {
+    return a.second < b.second; // Compare based on the second element
 }
 
-void exh_search(char** argv, vector<int> front, int k){
-  if(k==0){ //Si ha añadido todas las piezas 
-    best_L = L;
-    best_disp = disp;
+int phi_L(vector<Pair> n_list){
+  // Basada en la implementación del greedy, 
+  // dada una ordenación de las piezas las coloca
+  // para obtener una L.
+  vector<int> front(W, 0); 
+  for(Pair p : n_list){
 
-    auto end = chrono::steady_clock::now();
-    auto elapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
-    double elapsed_seconds = elapsed.count() / 1000.0;
+    bool been_put = false;
+    int a = p.first; int b = p.second;
 
-    //Escribir la solución
-    write_ans(argv, elapsed_seconds);
-  }
-  else{
-    if(valid()){ //poda
-      for(pair<Pair, int> bloc : n){
-        if (bloc.second > 0) //Si quedan piezas de la dimension
-        {
-          if(bloc.first.first == bloc.first.second){ // Si la pieza es cuadrada
-            add_piece(bloc.first, front); // update front, disp
-          }
-          else{
-            add_piece(bloc.first, front);
-            undo_changes(front, disp);
-            
-            add_piece({bloc.first.second,bloc.first.first},front);
-          }
-          exh_search(argv, front, k-1);
-          undo_changes(front, disp);
-        }
+    vector<Pair> order(front.size());
+    for (int i = 0; i < int(front.size()); ++i) order[i] = {i, front[i]};
+    // Ordenar de más bajo a más alto
+    sort(order.begin(), order.end(), compareBySecond);  
+    cout <<endl;
+
+    for (Pair pos : order){ //Buscar de debajo a arriba
+      int i = pos.first;
+      bool may_add_here = true;
+      int j = 0;
+      while (j <a && may_add_here){ // Si se puede añadir aquí
+        may_add_here = may_add_here && (front[i] >= front[i+j]) && i <= W-a;
+        ++j;
       }
+
+      if (!been_put && may_add_here){ //Añadir la pieza
+        disp.push_back({{i, front[i]},{i+a-1, front[i]+b-1}});
+        vector<int> new_front = front;
+        for (int j=0; j<a; ++j) new_front[i+j]= front[i]+b;
+        front = new_front;
+        been_put = true;
+      }
+
     }
+    
   }
+  // TODO Quitar la L y devolver directamente max
+  L = *max_element(front.cbegin(), front.cend());
+  return L;
+}
+
+void metah(char** argv){
+  vector< Pair > n_orig = {};
+  for(pair<Pair, int> blocs : n) {
+    for (int repes = 0; repes < blocs.second; repes++) n_orig.push_back(blocs.first);
+  }
+
+  reverse(n_orig.begin(), n_orig.end());
+  cout << "L orig: " << phi_L(n_orig) << endl;
+
 }
 
 int main(int argc, char** argv) {
@@ -109,19 +139,21 @@ int main(int argc, char** argv) {
     exit(0);
   }
 
-  int k; // Contador recursiu
   assert(argc == 3);
-  read_instance(argv, k);
+  read_instance(argv);
   
-  // Calcular una cota superior para realizar podas
-  for(pair<Pair, int> bloc : n){
-    best_L += bloc.second * min(bloc.first.first, bloc.first.second);
-  }
+  metah(argv);
 
-  // Longitud a la que se ha llegado en cada columna
-  vector<int> front(W, 0); 
+  // Mira el tiempo
+  auto end = chrono::steady_clock::now();
+  auto elapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
+  double elapsed_seconds = elapsed.count() / 1000.0;
 
-  exh_search(argv, front, k);
+  write_ans(argv, elapsed_seconds);
 
-
+  // Finalización de la busqueda
+  // auto end = chrono::steady_clock::now();
+  // auto elapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
+  // double elapsed_seconds = elapsed.count() / 1000.0;
+  cout << "Ha tardat en trobar totes les combinacions: " << elapsed_seconds<< endl;
 }
